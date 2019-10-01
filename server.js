b9137d52d47ea39284cefacf9094e41898dfba62
Zac Morton <zmorton8057@gmail.com>
2019-09-30 22:15:17 -0700

    Updated scraper to another website

--- server.js
+++ server.js
@@ -41,21 +41,21 @@ mongoose.connect("mongodb://localhost/unit18Populater", { useNewUrlParser: true 
 // A GET route for scraping the echoJS website
 app.get("/scrape", function (req, res) {
     // First, we grab the body of the html with axios
-    axios.get("http://www.echojs.com/").then(function (response) {
+    axios.get("http://www..gameinformer.com//").then(function (response) {
       // Then, we load that into cheerio and save it to $ for a shorthand selector
       var $ = cheerio.load(response.data);
   
       // Now, we grab every h2 within an article tag, and do the following:
-      $("article h2").each(function (i, element) {
+      $("article-title-page h2").each(function (i, element) {
         // Save an empty result object
         var result = {};
   
         // Add the text and href of every link, and save them as properties of the result object
         result.title = $(this)
-          .children("a")
+          .children("div")
           .text();
         result.link = $(this)
-          .children("a")
+          .children("div")
           .attr("href");
   
         // Create a new Article using the `result` object built from scraping
