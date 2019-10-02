2eea46464e99f83ac3e4e47d020e5aac7c828b4b
Zac Morton <zmorton8057@gmail.com>
2019-10-02 10:40:17 -0700

    changed the case sensitivity to article route

--- models/index.js
+++ models/index.js
@@ -1,7 +1,7 @@
 // Exporting an object containing all of our models
 
 module.exports = {
-    Article: require("./Article"),
+    Article: require("./article"),
     Note: require("./Note")
   };
   
