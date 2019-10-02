6ff667b381608cefb9ee08e45a60c61c5481e98d
Zac Morton <zmorton8057@gmail.com>
2019-10-02 10:46:10 -0700

    Replaced Spelling

--- models/index.js
+++ models/index.js
@@ -1,7 +1,7 @@
 // Exporting an object containing all of our models
 
 module.exports = {
-    Article: require("./article"),
-    Note: require("./Note")
+    Article: require("./articles"),
+    Note: require("./note")
   };
   
