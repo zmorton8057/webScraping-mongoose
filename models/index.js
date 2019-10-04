ef3feb7be8c0121c0814d808aafd1b437a4a6b1d
Zac Morton <zmorton8057@gmail.com>
2019-10-03 17:58:18 -0700

    Updated routes

--- models/index.js
+++ models/index.js
@@ -1,7 +1,7 @@
 // Exporting an object containing all of our models
 
 module.exports = {
-    Article: require("./articles"),
-    Note: require("./notes")
+    Article: require("./article"),
+    Note: require("./note")
   };
   
